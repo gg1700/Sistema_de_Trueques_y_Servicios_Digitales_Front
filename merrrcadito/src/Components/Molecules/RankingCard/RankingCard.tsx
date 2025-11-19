812aa5e0d3f1027da18fb6ebf64f3927a4b33336
jhosua <jhosua@debian-b.gold>
2025-11-19 00:47:14 -0400

    Actualizacion con el dev

--- merrrcadito/src/Components/Molecules/RankingCard/RankingCard.tsx
+++ merrrcadito/src/Components/Molecules/RankingCard/RankingCard.tsx
@@ -1,24 +1,37 @@
 import styles from './RankingCard.module.css'
 
 interface RankingCardProps{
+    cod_us: number,
     imagenUsuario: string | File,
     handlename: string,
     nombreUsuario: string,
     point: number | string,
+    puesto: number
 }
-export default function RankingCard(){
+export default function RankingCard({
+    cod_us,
+    imagenUsuario,
+    handlename,
+    nombreUsuario,
+    point,
+    puesto
+}:RankingCardProps){
     return(
         <div className={styles.containerCard}>
             <div className={styles.userImage}>
-                <img className={styles.imageUser}/>
+                <img 
+                  src={imagenUsuario}
+                  alt={cod_us.toString()}
+                  className={styles.imageUser}
+                />
             </div>
             <div>
                 <span>#</span>
                 <span className={styles.point}></span>
             </div>
             <div>
-                <h1 className={styles.handlename}></h1>
-                <h4 className={styles.userName}></h4>
+                <h1 className={styles.handlename}>{handlename}</h1>
+                <h4 className={styles.userName}>{nombreUsuario}</h4>
             </div>
         </div>
     );
